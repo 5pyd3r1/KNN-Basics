8895ffa5b670372ca6c62c3245244ea103336f0a
Michel Brink <michel.clan97@gmail.com>
2024-02-28 16:58:07 +0100

    Changed comments to English

--- KNN_wine/knn_wine.py
+++ KNN_wine/knn_wine.py
@@ -5,23 +5,21 @@ from sklearn.model_selection import train_test_split
 path = "..\Dataset\winequality-red.csv"
 data = pd.read_csv(path, delimiter=';')
 
-# Was vorhergesagt wird
+# What is predicted
 col_name = 'quality'
 
-# Spalte quality in int Form umwandeln
+# Convert the column 'quality' to int
 data[col_name] = data[col_name].astype('category')
 data[col_name] = data[col_name].cat.codes
 
-
-
-# Aufteilung der Tabellen (Input = data, Output = col)
+# Splitting the tables (Input = data, Output = col)
 col = data[col_name]
 data = data.drop([col_name], axis=1)
 
-# Aus den zwei Tabellen 4 Tabellen erzeugen für Train und Test Phase
+# Splitting the data into 4 tables for Train and Test Phase
 train_data, test_data, train_col, test_col = train_test_split(data, col, test_size=0.2)
 
-# Modell des KNN (1. Hidden layer 32 Knoten 2. Hidden layer 64 Knoten, 3. Output layer mit 3 Knoten)
+# Model of the KNN (1. Hidden layer 32 nodes 2. Hidden layer 120 nodes, 3. Hidden layer 120 nodes, 4. OL with 11 nodes)
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(32, activation=tf.nn.sigmoid, input_dim=11),
     tf.keras.layers.Dense(120, activation=tf.nn.sigmoid),
@@ -29,14 +27,14 @@ model = tf.keras.Sequential([
     tf.keras.layers.Dense(11, activation=tf.nn.softmax)
 ])
 
-# Konfiguration des Lernprozesses
+# Configuring the learning process
 model.compile(
-    optimizer='adam',   # Optimierungsfunktion adam
-    loss='sparse_categorical_crossentropy',     # Verlustfunktion
-    metrics=['accuracy']    # Anzahl richtiger Vorhersagen / Gesamtanzahl Vorhersagen
+    optimizer='adam',   # Optimization function adam
+    loss='sparse_categorical_crossentropy',     # Loss function
+    metrics=['accuracy']    # Number of correct predictions / Total number of predictions
 )
 
-# 70 Durchlaueufe
+# 70 runs
 model.fit(train_data, train_col, epochs=70)
 
 # Test Phase
