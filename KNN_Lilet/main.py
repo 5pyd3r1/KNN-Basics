1b9f8faa4a8d2040b941ac8972ee4b4b8c1adec0
Michel Brink <michel.clan97@gmail.com>
2024-02-28 17:02:16 +0100

    Changed comments to English

--- KNN_Lilet/main.py
+++ KNN_Lilet/main.py
@@ -5,35 +5,35 @@ from sklearn.model_selection import train_test_split
 path = "../Dataset/iris.csv"
 data = pd.read_csv(path, delimiter=',')
 
-# Was vorhergesagt wird
+# Predicted value
 col_name = 'species'
 
-# Spalte species in int Form umwandeln
+# Convert the column 'species' to int
 data[col_name] = data[col_name].astype('category')
 data[col_name] = data[col_name].cat.codes
 
-# Aufteilung der Tabellen (Input = data, Output = col)
+# Splitting the tables (Input = data, Output = col)
 col = data[col_name]
 data = data.drop([col_name], axis=1)
 
-# Aus den zwei Tabellen 4 Tabellen erzeugen für Train und Test Phase
+# Splitting the data into 4 tables for Train and Test Phase
 train_data, test_data, train_col, test_col = train_test_split(data, col, test_size=0.2)
 
-# Modell des KNN (1. Hidden layer 32 Knoten 2. Hidden layer 64 Knoten, 3. Output layer mit 3 Knoten)
+# Model of the KNN (1. Hidden layer 32 nodes 2. Hidden layer 64 nodes, 3. OL with 3 nodes)
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(32, activation=tf.nn.sigmoid, input_dim=4),
     tf.keras.layers.Dense(64, activation=tf.nn.sigmoid),
     tf.keras.layers.Dense(3, activation=tf.nn.softmax)
 ])
 
-# Konfiguration des Lernprozesses
+# Configuring the learning process
 model.compile(
-    optimizer='adam',   # Optimierungsfunktion adam
-    loss='sparse_categorical_crossentropy',     # Verlustfunktion
-    metrics=['accuracy']    # Anzahl richtiger Vorhersagen / Gesamtanzahl Vorhersagen
+    optimizer='adam',   # Optimizer function adam
+    loss='sparse_categorical_crossentropy',     # Loss function
+    metrics=['accuracy']    # Number of correct predictions / Total number of predictions
 )
 
-# 30 Durchlaueufe
+# 60 runs
 model.fit(train_data, train_col, epochs=60)
 
 # Test Phase
